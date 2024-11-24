b91adfee9eb3e90535972ac69dc1517d439c622e
Alex <leha.fedosow@gmail.com>
2024-11-24 20:23:23 +0300

    initial

---
+++ src/main/java/org/example/hwspringweb/HwSpringWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.hwspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSpringWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hwspringweb/HwSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hwspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
